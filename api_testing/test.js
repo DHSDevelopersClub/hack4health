2df56a1bab3451982943b08c578608f1d0285ca6
Max Starr <max.starr@techie.com>
2017-04-01 10:16:23 -0700

    Proper svg redraw

--- api_testing/test.js
+++ api_testing/test.js
@@ -4,15 +4,15 @@ var soda = require('soda-js');
 var consumer = new soda.Consumer('data.marincounty.org');
 
 // Attempt to query for 20 rows
-consumer.query()
-    // Choose SWITRS as DB
-    .withDataset('3k92-tyja')
-    .limit(5)
-    .where({ 'City': 'SAN RAFAEL' })
-    .where({ 'Case ID': 4565648 })
-    .getRows()
-        .on('success', function(rows) { console.log(rows); })
-        .on('error', function(error) { console.log(error); });
+// consumer.query()
+//     // Choose SWITRS as DB
+//     .withDataset('3k92-tyja')
+//     .limit(5)
+//     .where({ 'City': 'SAN RAFAEL' })
+//     .where({ 'Case ID': 4565648 })
+//     .getRows()
+//         .on('success', function(rows) { console.log(rows); })
+//         .on('error', function(error) { console.log(error); });
 
 var emq = function() {
     consumer.query()
@@ -22,3 +22,5 @@ var emq = function() {
             .on('success', function(rows) { console.log(rows); })
             .on('error', function(error) { console.log(error); });
 };
+
+emq();
--- client/script.js
+++ client/script.js
@@ -1,92 +1,111 @@
 var form = document.getElementById('dataSelector');
 form.addEventListener('change', changeDataset);
 
-//Map dimensions (in pixels)
-var width = 519,
-    height = 600;
+var draw = function (datasetName) {
+    //Map dimensions (in pixels)
+    var width = 519,
+        height = 600;
 
-//Map projection
-var projection = d3.geoMercator()
-    .scale(46567.862117552664)
-    .center([-122.72168747117493,38.09163738233181]) //projection center
-    .translate([width/2,height/2]) //translate to center the map in view
+    //Map projection
+    var projection = d3.geoMercator()
+        .scale(46567.862117552664)
+        .center([-122.72168747117493, 38.09163738233181]) //projection center
+        .translate([width / 2, height / 2]) //translate to center the map in view
 
-//Generate paths based on projection
-var path = d3.geoPath()
-    .projection(projection);
+    //Generate paths based on projection
+    var path = d3.geoPath()
+        .projection(projection);
 
-//Create an SVG
-var svg = d3.select("svg")
-    .attr("width", width)
-    .attr("height", height);
+    //Create an SVG
+    var svg = d3.select("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-//Group for the map features
-var features = svg.append("g")
-    .attr("class","features");
+    //Group for the map features
+    var features = svg.append("g")
+        .attr("class", "features");
 
-// Generate colors
-var color = d3.scaleLinear()
-    .domain([1, 30, 50, 60, 70, 120, 200, 300])
-    .range(d3.schemeBlues[9]);
+    // Generate colors
+    var color = d3.scaleLinear()
+        .domain([1, 30, 50, 60, 70, 120, 200, 300])
+        .range(d3.schemeBlues[9]);
 
-//Create zoom/pan listener
-//Change [1,Infinity] to adjust the min/max zoom scale
-var zoom = d3.zoom()
-    .scaleExtent([1, 10])
-    .on("zoom",zoomed);
+    /* Initialize tooltip */
+    tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .html(function (d) {
+            return ("<h1 class=\"w3-panel w3-blue\"> Rate: " + d.rate + "</h1>")
+        });
 
-svg.call(zoom);
+    /* Invoke the tip in the context of your visualization */
+    svg.call(tip);
 
-var datasets = {}; // Not a literal map
-datasets['responseTime'] = {};
-datasets['dispachTime'] = {};
-datasets['hospitalized'] = {};
-d3.queue()
-    .defer(d3.json, "zipcodes.geojson")
-    .defer(d3.tsv, "response_time.tsv", function(d) { datasets['responseTime'][d.zip_code] = d.value; })
-    .defer(d3.tsv, "dispach_time.tsv", function(d) { datasets['dispachTime'][d.zip_code] = d.value; })
-    .defer(d3.tsv, "hospitalized.tsv", function(d) { datasets['hospitalized'][d.zip_code] = d.value; })
-    .await(ready);
+    //Create zoom/pan listener
+    //Change [1,Infinity] to adjust the min/max zoom scale
+    var zoom = d3.zoom()
+        .scaleExtent([1, 10])
+        .on("zoom", zoomed);
+
+    svg.call(zoom);
+
+    var datasets = {}; // Not a literal map
+    datasets['responseTime'] = {};
+    datasets['dispachTime'] = {};
+    datasets['hospitalized'] = {};
+    d3.queue()
+        .defer(d3.json, "zipcodes.geojson")
+        .defer(d3.tsv, "response_time.tsv", function (d) {
+            datasets['responseTime'][d.zip_code] = d.value;
+        })
+        .defer(d3.tsv, "dispach_time.tsv", function (d) {
+            datasets['dispachTime'][d.zip_code] = d.value;
+        })
+        .defer(d3.tsv, "hospitalized.tsv", function (d) {
+            datasets['hospitalized'][d.zip_code] = d.value;
+        })
+        .await(ready);
 
 
 
-function ready(error,geodata) {
-  if (error) console.log(error); //unknown error, check the console
+    function ready(error, geodata) {
+        if (error) console.log(error); //unknown error, check the console
 
-  //Create a path for each map feature in the data
-  features.selectAll("path")
-    .data(geodata.features)
-    .enter()
-    .append("path")
-      .attr("d",path)
-      .attr("fill", function(d) { return color(d.rate = datasets['responseTime'][d.properties.zip_code_geo]); })
-    .on("click",clicked);
+        console.log(datasets);
+
+        //Create a path for each map feature in the data
+        features.selectAll("path")
+            .data(geodata.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr("fill", function (d) {
+                return color(d.rate = datasets[datasetName][d.properties.zip_code_geo]);
+            })
+            .on("click", clicked)
+            .on('mouseover', tip.show)
+            .on('mouseout', tip.hide)
+
+    }
 
 }
 
 function changeDataset(e) {
-  datasetName = e.target.id;
-  features.selectAll("path")
-    .attr("fill", function(d) {
-      if (datasets[datasetName]) {
-        return color(datasets[datasetName][d.properties.zip_code_geo]);
-      }
-      else {
-        return '#000000';
-      }
-    })
+    datasetName = e.target.id;
+    draw(datasetName);
 }
 
 // Add optional onClick events for features here
 // d.properties contains the attributes (e.g. d.properties.name, d.properties.population)
-function clicked(d,i) {
-  console.log(d);
-  console.log(i);
+function clicked(d, i) {
+    console.log(d);
+    console.log(i);
 }
 
 
 //Update map on zoom/pan
 function zoomed() {
-  // console.log(d3.event.transform);
- svg.attr("transform", d3.event.transform);
+    // console.log(d3.event.transform);
+    svg.attr("transform", d3.event.transform);
 }
+
+draw("dispachTime");
