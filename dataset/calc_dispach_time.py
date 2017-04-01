ef4871b6643a786a1b4f61949bfdd547b364214c
Duncan Grubbs <duncan.grubbs@gmail.com>
2017-04-01 09:05:32 -0700

    Make generic delta time function.

--- dataset/calc_dispach_time.py
+++ dataset/calc_dispach_time.py
@@ -4,7 +4,26 @@ import datetime
 DATA = pd.read_csv('data.csv')
 INCEDENTS = DATA.iterrows()
 
-zip_codes = {}
+def calc_delta_time(start, end, export_url):
+    zip_codes = {}
+
+    for incident in DATA.iterrows():
+        zip_code = incident[1]['Incident ZIP Postal']
+        first_time = datetime.datetime.strptime(incident[1][start], '%m/%d/%Y %I:%M:%S %p')
+        second_time = datetime.datetime.strptime(incident[1][end], '%m/%d/%Y %I:%M:%S %p')
+        delta_time = (second_time - first_time)
+        if resonable(delta_time):
+            if zip_code in zip_codes:
+                zip_codes[zip_code]['total_time']+=delta_time
+                zip_codes[zip_code]['num_incedents']+=1
+            else:
+                zip_codes[zip_code] = {'total_time': delta_time, 'num_incedents': 1}
+
+    for z in zip_codes:
+        zip_codes[z] = (zip_codes[z]['total_time'] / zip_codes[z]['num_incedents']).seconds
+
+    dataframe = pd.DataFrame(zip_codes.items(), columns=['zip_code', 'value'])
+    dataframe.to_csv(export_url, sep='\t', index_label=False, index=False)
 
 def resonable(time):
     if time < datetime.timedelta(seconds=0):
@@ -15,20 +34,4 @@ def resonable(time):
         return False
     return True
 
-for incident in DATA.iterrows():
-    zip_code = incident[1]['Incident ZIP Postal']
-    dispached = datetime.datetime.strptime(incident[1]["Time Vehicle was Dispatched"], '%m/%d/%Y %I:%M:%S %p')
-    callReceived = datetime.datetime.strptime(incident[1]["Time Call Was Received"], '%m/%d/%Y %I:%M:%S %p')
-    resp_time = (dispached - callReceived)
-    if resonable(resp_time):
-        if zip_code in zip_codes:
-            zip_codes[zip_code]['total_time']+=resp_time
-            zip_codes[zip_code]['num_incedents']+=1
-        else:
-            zip_codes[zip_code] = {'total_time': resp_time, 'num_incedents': 1}
-
-for z in zip_codes:
-    zip_codes[z] = (zip_codes[z]['total_time'] / zip_codes[z]['num_incedents']).seconds
-
-dataframe = pd.DataFrame(zip_codes.items(), columns=['zip_code', 'value'])
-dataframe.to_csv('../client/dispach_time.tsv', sep='\t', index_label=False, index=False)
+calc_delta_time("Time Call was Recieved", "Time Arrived on Scene", "./arrived.tsv")
